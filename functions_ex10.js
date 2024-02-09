ebc81e98a4a368b324cf047a9389f79d60862aeb
inothi <inothi.tomek@gmail.com>
2024-02-09 15:31:13 +0100

    add final solution for ex.2 and ex.3

--- functions_ex10.js
+++ functions_ex10.js
@@ -1,16 +1,30 @@
+// ex.1
 function dodajNowyWpis() {
     let tableRows = document.getElementById("wierszetabeli");
     let howManyRows = tableRows.children.length;
-    let date = new Date().toISOString().slice(0, 10);
+    let date = new Date().toISOString().substring(0, 10);
     let userDescription = prompt("Na co zostały wydane pieniądze?");
-    let moneySpent = Number(prompt(`Jaką kwotę wydano na ${userDescription}`)).toFixed(2);
+
+    // ex.3
+    let userDescriptionTest = /[A-z,0-9]/i.test(userDescription);
+    if ((userDescription.length == 0) || (userDescriptionTest == false)) {
+        console.log(typeof(userDescriptionTest));
+        alert("Wpis jest pusty, operację anulowano");
+        return;
+    };
+    let moneySpent = Number(prompt(`Jaką kwotę wydano na ${userDescription}`).replace(",", ".")).toFixed(2);
+    if (isNaN(moneySpent)) {
+        alert("Nie podano właściwej wartości liczbowej, operację anulowano");
+        return;
+    };
+    // end ex.3
 
     let newRow = document.createElement("tr");
 
     let col1 = document.createElement("td");
     col1.setAttribute("scope","col");
     col1.innerHTML = howManyRows + 1;
-    newRow.appendChild(col1);
+    newRow.append(col1);
 
     let col2 = document.createElement("td");
     col2.setAttribute("scope","col");
@@ -28,4 +42,33 @@ function dodajNowyWpis() {
     newRow.appendChild(col4);
 
     tableRows.appendChild(newRow);
+    
+    sumAllCost();
+    sumCostOfCurrentMonth();
 };
+// end ex.1
+
+// ex.2
+function sumAllCost() {
+    let tableRows = document.getElementById("wierszetabeli").children;
+    let tableValue = 0;
+    for (let i = 0; i < tableRows.length; i++) {
+        tableValue += Number(tableRows[i].children[3].innerHTML);
+    };
+    document.getElementById("sumaWydatkow").value = tableValue.toFixed(2);
+};
+
+function sumCostOfCurrentMonth() {
+    let tableRows = document.getElementById("wierszetabeli").children;
+    let currentMonth = new Date().toISOString().substring(4, 8);
+    let currentMonthValue = 0;
+    for (let i = 0; i < tableRows.length; i++) {
+        if (tableRows[i].children[1].innerHTML.includes(currentMonth)) {
+            currentMonthValue += Number(tableRows[i].children[3].innerHTML);
+        };
+    };
+    document.getElementById("wydatkiBiezacegoMiesiaca").value = currentMonthValue.toFixed(2);
+};
+sumAllCost();
+sumCostOfCurrentMonth();
+// end ex.2
