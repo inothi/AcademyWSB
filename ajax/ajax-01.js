0814fa524dad6a52e1f1988e4ef531a71f3514a9
inothi <inothi.tomek@gmail.com>
2024-03-18 09:18:18 +0100

    initial commit

---
+++ ajax/ajax-01.js
@@ -0,0 +1,18 @@
+/* ---------------------------------------- //
+// Zadanie 1 - Dane krajów - XMLHttpRequest //
+// ---------------------------------------- //
+
+Korzystając z serwisu "https://restcountries.com/" rozbuduj poniższą stronę o następujące funkcje:
+
+1. Podczas uruchamiania strony uzupełnij listę rozwijalną o wszystkie kraje świata. Skorzystaj z url "https://restcountries.com/v2/all"
+
+2. Utwórz funkcję, która po wybraniu kraju z listy rozwijalnej, uzupełni tabelę z danymi danego kraju. 
+Funkcję podepnij pod zdarzenie "change" kontrolki "select" Skorzystaj z url "https://restcountries.com/v2/name/nazwaKraju", 
+gdzie nazwaKraju zastępujesz nazwą wybranego kraju. Zadanie wykonaj z wykorzystaniem obiektu XMLHttpRequest.
+
+Nie modyfikuj pliku HTML.
+
+// ---------------------------------------- //
+// Zadanie 1 - Dane krajów - XMLHttpRequest //
+// ---------------------------------------- */
+
