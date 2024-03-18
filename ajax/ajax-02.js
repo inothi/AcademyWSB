d874d073f896c99550fcc3069ded32f7178f2995
inothi <inothi.tomek@gmail.com>
2024-03-18 12:45:18 +0100

    initial commit

---
+++ ajax/ajax-02.js
@@ -0,0 +1,22 @@
+/* ----------------------------------- //
+// Zadanie 1 - Dane krajów - Fetch API //
+// ----------------------------------- //
+
+Korzystając z serwisu "https://restcountries.com/" rozbuduj poniższą stronę o następujące funkcje:
+
+1. Podczas uruchamiania strony uzupełnij listę rozwijalną o wszystkie kraje świata. Skorzystaj z url "https://restcountries.com/v2/all"
+
+2. Utwórz funkcję, która po wybraniu kraju z listy rozwijalnej, uzupełni tabelę z danymi danego kraju. 
+Funkcję podepnij pod zdarzenie "change" kontrolki "select" Skorzystaj z url "https://restcountries.com/v2/name/nazwaKraju", 
+gdzie nazwaKraju zastępujesz nazwą wybranego kraju. 
+
+Zadanie wykonaj z wykorzystaniem metody Fetch API.
+
+Nie modyfikuj pliku HTML.
+
+
+// --------- //
+// SOLUTIONS //
+// --------- */
+
+
