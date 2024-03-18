8560d6768a1bd7bb9b14075dad472e9c74717fc3
inothi <inothi.tomek@gmail.com>
2024-03-18 13:55:09 +0100

    initial commit

---
+++ ajax/ajax-05.js
@@ -0,0 +1,24 @@
+/*-------------------------------//
+//  Zadanie 5 - Prognoza pogody  //
+//-------------------------------//
+
+Wykorzystując jedną z poznanych technologii AJAX, przy wykorzystaniu API serwisu „https://openweathermap.org/” wykonaj poniższe polecenia:
+
+1. Uzupełnij pole „select” o wybrane polskie miasta: Warszawa, Gdańsk, Poznań, Kraków oraz Wrocław.
+2. Utwórz połączenie AJAX wykorzystując adres URL:
+https://api.openweathermap.org/data/2.5/weather?q=nazwaMiasta&units=metric&appid=cf608d877811f21a9017c72939080a39
+gdzie fragment nazwaMiasta zastąpi nazwa wybranego w polu „select” miasta. 
+Przeanalizuj dokładnie odpowiedź z serwera. Dodatkowo możesz wspomóc się dokumentacją ze strony https://openweathermap.org/current. 
+Połączenie ma się wykonywać każdorazowo po zmianie wartości w polu „select”.
+
+3. Wykorzystując dane z połączenia AJAX, uzupełnij dane w poniżej tabeli tj. nazwę miasta, opis bieżącej pogody, temperaturę, 
+wilgotność, wiatr oraz ciśnienie. Uzupełnij także ikonę symbolizującą bieżącą pogodę wykorzystując  link https://openweathermap.org/img/wn/kod@2x.png 
+gdzie słowo „kod” zastąpi kod bieżącej pogody znajdujący się w odpowiedzi z serwera.
+
+Nie modyfikuj pliku HTML
+
+//-------------//
+//  SOLUTIONS  //
+//-------------*/
+
+
